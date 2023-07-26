995cf67cb4740589a4b5d786c39419866c3e1d19
Maciej Marut <mcgtrt@gmail.com>
2023-07-26 17:03:18 +0100

    HTTP metrics and business logic separation

---
+++ aggregator/http.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/mcgtrt/toll-tracker/types"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+type HTTPMetricHandler struct {
+	reqCounter prometheus.Counter
+}
+
+func newHTTPMetricHandler(reqName string) *HTTPMetricHandler {
+	reqCounter := promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: fmt.Sprintf("http_%s_request_counter", reqName),
+		Name:      "aggregator",
+	})
+	return &HTTPMetricHandler{
+		reqCounter: reqCounter,
+	}
+}
+
+func (h *HTTPMetricHandler) instrument(next http.HandlerFunc) http.HandlerFunc {
+	h.reqCounter.Inc()
+	return func(w http.ResponseWriter, r *http.Request) {
+		next(w, r)
+	}
+}
+
+func handleInvoice(service Aggregator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		values, ok := r.URL.Query()["obu"]
+		if !ok {
+			writeJSON(w, http.StatusBadRequest, errJSON(fmt.Errorf("obu not provided")))
+			return
+		}
+		obuid, err := strconv.Atoi(values[0])
+		fmt.Printf("OBUID: %d\n", obuid)
+		if err != nil {
+			writeJSON(w, http.StatusBadRequest, errJSON(err))
+			return
+		}
+		inv, err := service.CalculateInvoice(obuid)
+		fmt.Printf("INV: %v, ERR: %v", inv, err)
+		if err != nil {
+			writeJSON(w, http.StatusBadRequest, errJSON(err))
+			return
+		}
+		writeJSON(w, http.StatusOK, inv)
+	}
+}
+
+func handleAggregate(service Aggregator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var dist types.Distance
+		if err := json.NewDecoder(r.Body).Decode(&dist); err != nil {
+			writeJSON(w, http.StatusBadRequest, errJSON(err))
+			return
+		}
+		if err := service.AggregateDistance(dist); err != nil {
+			writeJSON(w, http.StatusInternalServerError, errJSON(err))
+			return
+		}
+	}
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func errJSON(err error) map[string]string {
+	return map[string]string{"err": err.Error()}
+}
--- aggregator/main.go
+++ aggregator/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/mcgtrt/toll-tracker/types"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
@@ -49,58 +48,18 @@ func makeGRPCTransport(listenAddr string, srv Aggregator) error {
 }
 
 func makeHTTPTransport(listenAddr string, service Aggregator) error {
-	http.HandleFunc("/aggregate", handleAggregate(service))
-	http.HandleFunc("/invoice", handleInvoice(service))
+	var (
+		aggMetricHandler = newHTTPMetricHandler("aggregate")
+		invMetricHandler = newHTTPMetricHandler("invoice")
+	)
+
+	logrus.Infof("HTTP Transport running on port %s\n", listenAddr)
+
+	http.HandleFunc("/aggregate", aggMetricHandler.instrument(handleAggregate(service)))
+	http.HandleFunc("/invoice", invMetricHandler.instrument(handleInvoice(service)))
 	http.Handle("/metrics", promhttp.Handler())
-	fmt.Printf("HTTP Transport running on port %s\n", listenAddr)
+
 	return http.ListenAndServe(listenAddr, nil)
-}
-
-func handleInvoice(service Aggregator) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		values, ok := r.URL.Query()["obu"]
-		if !ok {
-			writeJSON(w, http.StatusBadRequest, errJSON(fmt.Errorf("obu not provided")))
-			return
-		}
-		obuid, err := strconv.Atoi(values[0])
-		fmt.Printf("OBUID: %d\n", obuid)
-		if err != nil {
-			writeJSON(w, http.StatusBadRequest, errJSON(err))
-			return
-		}
-		inv, err := service.CalculateInvoice(obuid)
-		fmt.Printf("INV: %v, ERR: %v", inv, err)
-		if err != nil {
-			writeJSON(w, http.StatusBadRequest, errJSON(err))
-			return
-		}
-		writeJSON(w, http.StatusOK, inv)
-	}
-}
-
-func handleAggregate(service Aggregator) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var dist types.Distance
-		if err := json.NewDecoder(r.Body).Decode(&dist); err != nil {
-			writeJSON(w, http.StatusBadRequest, errJSON(err))
-			return
-		}
-		if err := service.AggregateDistance(dist); err != nil {
-			writeJSON(w, http.StatusInternalServerError, errJSON(err))
-			return
-		}
-	}
-}
-
-func writeJSON(w http.ResponseWriter, status int, v any) error {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(v)
-}
-
-func errJSON(err error) map[string]string {
-	return map[string]string{"err": err.Error()}
 }
 
 func init() {
